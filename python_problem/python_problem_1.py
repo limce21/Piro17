ea47ae1530a03c21b3b389321a9d5d8c8b073cbe
chaenLIM <limce21@naver.com>
2022-07-08 07:05:31 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -14,3 +14,18 @@ while(1):
 for i in range(x):
     num += 1
     print("PlayerA :", num)
+
+while(1):
+    try:
+        x = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        if x < 1 or x > 3:
+            print('1,2,3 중 하나를 입력하세요')
+            continue
+        else:
+            break
+    except:
+        print("정수를 입력하세요")
+
+for i in range(x):
+    num += 1
+    print("PlayerB :", num)
